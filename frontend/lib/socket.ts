369bcd2464b6b35a2ec3d309f22d9fe292cda740
PIO-VIA <piodjiele@gmail.com>
2025-11-05 09:20:32 +0100

    add the new version of socket

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,4 +1,4 @@
-import { io, Socket } from 'socket.io-client';
+import {  io, Socket } from 'socket.io-client';
 import type { ServerToClientEvents, ClientToServerEvents } from '@/types/socket';
 
 let socket: Socket<ServerToClientEvents, ClientToServerEvents> | null = null;
